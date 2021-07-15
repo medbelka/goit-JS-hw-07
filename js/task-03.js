2d579025d60277b3a279f5f52db012098099a2e1
medbelka <margopv@gmail.com>
2021-07-15 15:44:04 +0300

    update task-03

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,26 @@ const images = [
       alt: 'Group of Horses Running',
     },
   ];
+
+  const makeImagesListMarkup = ({url, alt}) => {
+    return `
+    <li>
+    <img src=${url} alt=${alt}</li>
+    `
+  };
+  const makeImageMarkup = images.map(makeImagesListMarkup).join('');
+    const galleryContainerRef = document.querySelector("#gallery");
+galleryContainerRef.insertAdjacentHTML('beforeend',makeImageMarkup);
+
+// const createImagesRef = ({url, alt}) =>{
+  //   const liRef = document.createElement('li');
+  //   const imgRef = document.createElement('img');
+  //   imgRef.src = url;
+  //   imgRef.alt = alt;
+  //   liRef.append(imgRef);
+  //   console.log(liRef);
+  //   return liRef;
+  // }
+
+  // const imageElements = images.map(createImagesRef)
+  // galleryContainerRef.append(...imageElements)
